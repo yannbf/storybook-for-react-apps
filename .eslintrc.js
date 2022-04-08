5e34d2eb93a024a66f996c3b2238d20ad16cc921
Yann Braga <yannbf@gmail.com>
2022-04-08 20:03:13 +0200

    [begin] module 02: MealDrop - accessing the project

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb-typescript',
-    'plugin:prettier/recommended',
-    'plugin:storybook/recommended',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb-typescript', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,10 @@
 module.exports = {
-  staticDirs: ['../public'],
   stories: [
     '../src/docs/Introduction.stories.mdx',
     '../src/docs/*.stories.mdx',
     '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-a11y',
-    'storybook-addon-designs',
-    'storybook-mobile',
-  ],
-  features: {
-    storyStoreV7: true,
-    interactionsDebugger: true,
-  },
+  addons: ['@storybook/addon-essentials', '@storybook/preset-create-react-app'],
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
